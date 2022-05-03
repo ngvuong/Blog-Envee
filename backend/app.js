8abd493cf419cfb6fd158f395dbd6c733cf8b47e
ngvuong <vminguyen@ucdavis.edu>
2022-05-03 02:28:16 -0700

    Update app, remove helmet, cors

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const compression = require('compression');
-const helmet = require('helmet');
-const cors = require('cors');
 require('dotenv').config();
 require('./config/passport');
 
@@ -20,10 +18,8 @@ const app = express();
 connectDB();
 
 // Middlewares
-app.use(cors());
 app.use(logger('dev'));
 app.use(compression());
-app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
